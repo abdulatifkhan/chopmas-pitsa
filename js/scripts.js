682fd02d90c5c3bb90762ae9f8967f4b26f2a679
abdulatifkhan <anvarkhanov96@gmail.com>
2020-11-09 15:55:46 +0500

    Add js options

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ var breadTypes = ['Yupqa', `O'rtacha`, 'Qalin', 'Qolipli non'];
 var sizes = [25, 30, 35];
 var toppings = ['Pomidor', 'Tuzlangan bodring', `Kurka go'shti`, `Qo'ziqorin`, 'Zaytun', 'Qazi'];
 var addl = ['Achchiq', 'Sosiskali'];
+
+var elPizzaForm = document.querySelector('.js-pizza-form');
+var elBreadSelect = elPizzaForm.querySelector('.js-bread-select');
+
+
+for (var i = 0; i < breadTypes.length; i++) {
+  var elNewBreadOption = document.createElement('option');
+  elNewBreadOption.textContent = breadTypes[i];
+
+  elBreadSelect.appendChild(elNewBreadOption);
+}
