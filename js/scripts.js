3a558200c1df6ce3949ff4231258c4337679646c
abdulatifkhan <anvarkhanov96@gmail.com>
2020-11-09 15:31:52 +0500

    Add var elements

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var breadTypes = ['Yupqa', `O'rtacha`, 'Qalin', 'Qolipli non'];
+var sizes = [25, 30, 35];
+var toppings = ['Pomidor', 'Tuzlangan bodring', `Kurka go'shti`, `Qo'ziqorin`, 'Zaytun', 'Qazi'];
+var addl = ['Achchiq', 'Sosiskali'];
