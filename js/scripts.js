d3396326e7485c3c6b7bd2e3bd8109ec1ddc7fd6
abdulatifkhan <anvarkhanov96@gmail.com>
2020-11-09 16:54:58 +0500

    Add left side of js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,61 @@ var addl = ['Achchiq', 'Sosiskali'];
 
 var elPizzaForm = document.querySelector('.js-pizza-form');
 var elBreadSelect = elPizzaForm.querySelector('.js-bread-select');
-
+var elSizeRadioWrapper = elPizzaForm.querySelector('.js-size-radio-wrapper');
+var elToppingCheckboxWrapper = elPizzaForm.querySelector('.js-topping-checkbox-wrapper');
+var elAddlIngredientWrapper = elPizzaForm.querySelector('.js-addl-ingredient-wrapper');
 
 for (var i = 0; i < breadTypes.length; i++) {
   var elNewBreadOption = document.createElement('option');
   elNewBreadOption.textContent = breadTypes[i];
 
   elBreadSelect.appendChild(elNewBreadOption);
+}
+
+for (var i = 0; i < sizes.length; i++) {
+  var elSizeLabel = document.createElement('label');
+  var elSizeInput = document.createElement('input');
+  var elSizeSpan = document.createElement('span');
+
+  elSizeInput.type = 'radio';
+  elSizeInput.name = 'size';
+  elSizeInput.classList.add('size-radio', 'js-size-radio', 'ml-3');
+  elSizeInput.value = sizes[i];
+
+  elSizeSpan.textContent = `${sizes[i]} sm`;
+
+  elSizeLabel.append(elSizeInput, elSizeSpan);
+  elSizeRadioWrapper.appendChild(elSizeLabel);
+}
+
+for (var i = 0; i < toppings.length; i++) {
+  var elToppingLabel = document.createElement('label');
+  var elToppingInput = document.createElement('input');
+  var elToppingSpan = document.createElement('span');
+
+  elToppingInput.type = 'checkbox';
+  elToppingInput.name = 'size';
+  elToppingInput.classList.add('topping-checkbox', 'js-topping-checkbox', 'ml-3');
+  elToppingInput.value = sizes[i];
+
+  elToppingSpan.textContent = toppings[i];
+
+  elToppingLabel.append(elToppingInput, elToppingSpan);
+  elToppingCheckboxWrapper.appendChild(elToppingLabel);
+}
+
+for (var i = 0; i < addl.length; i++) {
+  var elAddlLabel = document.createElement('label');
+  var elAddlInput = document.createElement('input');
+  var elAddlSpan = document.createElement('span');
+
+  elAddlInput.type = 'checkbox';
+  elAddlInput.name = 'size';
+  elAddlInput.classList.add('addl-ingredient', 'js-addl-ingredient', 'ml-3');
+  elAddlInput.value = addl[i];
+
+  elAddlSpan.textContent = addl[i];
+
+  elAddlLabel.append(elAddlInput, elAddlSpan);
+  elAddlIngredientWrapper.appendChild(elAddlLabel);
 }
